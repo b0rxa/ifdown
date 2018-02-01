14c966bb16d4daa550d505ed7ee05e9009013b1c
b0rxa <borja.calvo@gmail.com>
2018-02-01 13:49:13 +0100

    First upload

---
+++ R/ifirak.R
@@ -0,0 +1,11 @@
+ifirak <- function(..., keep_tex = TRUE, citation_package='natbib'){
+  template <- system.file(file.path("rmarkdown", "templates", "ifirak", "resources", "template.tex"),
+              package="ifdown")
+
+  fmt <- rmarkdown::pdf_document(template = template,
+                                 keep_tex = keep_tex,
+                                 citation_package = citation_package,
+                                 ...)
+  fmt$inherits <- "pdf_document"
+  return(fmt)
+}
