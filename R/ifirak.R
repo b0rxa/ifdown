2a95e001dfcc82e94a6a01f9b9098046b4fc4e5a
Borja <borja.calvo@gmail.com>
2019-02-07 11:39:28 +0100

    bug correction

--- R/ifirak.R
+++ R/ifirak.R
@@ -10,6 +10,18 @@ ifirak <- function(..., keep_tex = TRUE, citation_package='natbib'){
   return(fmt)
 }
 
+ifirak <- function(..., keep_tex = TRUE, citation_package='natbib'){
+  template <- system.file(file.path("rmarkdown", "templates", "ifirak2", "resources", "template.tex"),
+                          package="ifdown")
+
+  fmt <- rmarkdown::pdf_document(template = template,
+                                 keep_tex = keep_tex,
+                                 citation_package = citation_package,
+                                 ...)
+  fmt$inherits <- "pdf_document"
+  return(fmt)
+}
+
 ifreport <- function(..., keep_tex = TRUE, citation_package='natbib'){
   template <- system.file(file.path("rmarkdown", "templates", "ifreport", "resources", "template.tex"),
                           package="ifdown")
