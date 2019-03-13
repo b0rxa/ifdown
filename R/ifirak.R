01e7b37286f6acaa797547bbd53876271c4b14ed
Borja <borja.calvo@gmail.com>
2019-03-13 15:28:32 +0100

    Ifreport2 added

--- R/ifirak.R
+++ R/ifirak.R
@@ -33,3 +33,16 @@ ifreport <- function(..., keep_tex = TRUE, citation_package='natbib'){
   fmt$inherits <- "pdf_document"
   return(fmt)
 }
+
+
+ifreport2 <- function(..., keep_tex = TRUE, citation_package='natbib'){
+  template <- system.file(file.path("rmarkdown", "templates", "ifreport2", "resources", "template.tex"),
+                          package="ifdown")
+
+  fmt <- rmarkdown::pdf_document(template = template,
+                                 keep_tex = keep_tex,
+                                 citation_package = citation_package,
+                                 ...)
+  fmt$inherits <- "pdf_document"
+  return(fmt)
+}
